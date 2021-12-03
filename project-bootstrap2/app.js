1a27423022bf6a8c456845940c626d7132f5b958
DanyaHomenko <74784472+DanyaHomenko@users.noreply.github.com>
2021-12-03 21:06:29 +0200

    Add files via upload

---
+++ project-bootstrap2/app.js
@@ -0,0 +1,12 @@
+(function(){
+    const header = document.qwertySelector('.header');
+    window.onscroll = () => {
+        if(window.pageYOffset > 50){
+            header.classList.add('header_active');
+        }
+        else{
+            header.classList.remove('header_active');
+        }
+    }
+}
+);
